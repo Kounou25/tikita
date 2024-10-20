f82d8a09dccb0bc316069a6309b67980b0a3900a
Kounou Gnimassou Gilbert <kounougilbert@MacBook-Air-de-Kounou.local>
2024-10-20 01:24:38 +0100

    back to safe zone gilbert

--- app/page.js
+++ app/page.js
@@ -93,7 +93,7 @@ export default function Home() {
 
       <footer>
         <p><strong>TIKITA PRO V1</strong></p>
-        <p>Créée avec ❤️ par <a href="https://wa.me/88715276" target="_blank">Kounou Gilberto</a></p>
+        <p>Créée avec ❤️ par <a href="https://wa.me/88715276" target="_blank">Kounou Gilbert</a></p>
         </footer>
 
       <style jsx>{`
